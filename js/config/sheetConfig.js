5fa342df80b6eb55b6a13da973ff95e1b06b2ddb
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-24 13:29:44 +0530

    Update sheetConfig.js

--- js/config/sheetConfig.js
+++ js/config/sheetConfig.js
@@ -1,3 +1,5 @@
 export const SHEET_URLS = {
-  STYLE_SUMMARY: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pubhtml?gid=397455584&single=true"
+  STYLE_SUMMARY: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pub?gid=397455584&single=true&output=csv",
+  DATA: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pub?gid=1097825949&single=true&output=csv",
+  CHANNEL_NAME: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pub?gid=310670653&single=true&output=csv"
 };
