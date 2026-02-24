bedf0eac4dea2679ad40f68fdbda0b5580d1add6
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-24 13:26:57 +0530

    Update sheetConfig.js

--- js/config/sheetConfig.js
+++ js/config/sheetConfig.js
@@ -1,1 +1,3 @@
-// Google Sheet configuration URLs
+export const SHEET_URLS = {
+  STYLE_SUMMARY: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pubhtml?gid=397455584&single=true"
+};
